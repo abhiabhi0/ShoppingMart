3652b35f778e954c68af2473a791273ce475aea5
programmercave0 <mailabhi593@gmail.com>
2022-08-09 11:58:09 +0530

    exception added

---
+++ src/main/java/com/programmercave/Exception/ShoppingMartException.java
@@ -0,0 +1,12 @@
+package com.programmercave.Exception;
+
+public class ShoppingMartException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShoppingMartException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/programmercave/ShoppingMart/App.java
+++ src/main/java/com/programmercave/ShoppingMart/App.java
@@ -6,7 +6,6 @@ import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.infy.userInterface.Tester;
 import com.programmercave.Model.Customer;
 import com.programmercave.validator.Validator;
 /**
